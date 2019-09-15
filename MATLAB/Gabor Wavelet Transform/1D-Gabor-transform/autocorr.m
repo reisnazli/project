29052b016e167cd1c80cf48c537d622afa589ff9
reisnazli <46015528+reisnazli@users.noreply.github.com>
2019-09-15 14:38:58 +0900

    Update autocorr.m

--- MATLAB/Gabor Wavelet Transform/1D-Gabor-transform/autocorr.m
+++ MATLAB/Gabor Wavelet Transform/1D-Gabor-transform/autocorr.m
@@ -1,11 +1,7 @@
 function [A, A2] =  autocorr(M, alpha)
-%
-% computes the autocorrelation filter A(z) for the fractional B-spline.
-% A2(z) = A(z^2).
-%
+
 nu = (0: 1: M-1)/M;
 
-% N-term approximation
 N  = 150;		    
 			
 A =  zeros(1, M);
